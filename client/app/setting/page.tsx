861b6cad7fa9c9dab16eba8426f0ae1ba44a9a27
letuan279 <latuan388@gmail.com>
2023-12-13 12:14:50 +0700

    Fe + Be: setting page

--- client/app/setting/page.tsx
+++ client/app/setting/page.tsx
@@ -1,29 +1,150 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import apiClient from "@/api/apiClient";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "../../components/ui/alert-dialog";
+import { toast } from "react-toastify";
+import { fetchTasks } from "@/redux/taskSlice";
+import { fetchCategories } from "@/redux/categoriesSlice";
 
 function Setting() {
+  const responseGoogle = async () => {
+    const res = await apiClient.get("/tasks/auth-google");
+    const resData = await res.data;
+    window.location.replace(resData);
+  };
+
+  const { data } = useSelector((state: RootState) => state.user);
+  const [open, setOpen] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get("code");
+
+  useEffect(() => {
+    const asyncFunc = async () => {
+      if (code && data?._id) {
+        const res = await apiClient.get("/tasks/google-task" + "?code=" + code);
+        const resData = await res.data;
+        setTasks(resData);
+        window.history.pushState({ a: 1 }, "Page", "/setting"); // return to /setting
+        setOpen(true);
+      }
+    };
+    asyncFunc();
+  }, [data?._id, code]);
+
+  const handleAddBulkTask = async () => {
+    if (tasks.length === 0) return;
+
+    try {
+      const taskClean = tasks.map((item) => {
+        return {
+          name: item.summary,
+          start_day: item.start.dateTime,
+          end_day: item.end.dateTime,
+          color: "green",
+          description: item.description,
+          status: 1,
+          priority: 1,
+        };
+      });
+      // const taskClean = tasks;
+      // console.log(taskClean);
+
+      function makeid(length) {
+        let result = "";
+        const characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+          result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+          );
+          counter += 1;
+        }
+        return result;
+      }
+
+      const categoryName = "CATEGORY-" + makeid(5);
+      const res = await apiClient.post(`tasks/createBulk/${categoryName}`, {
+        tasks: taskClean,
+      });
+
+      const resData = await res.data;
+
+      if (resData.code === "20100") {
+        toast.success("Addition was successful!");
+        dispatch(fetchTasks());
+        dispatch(fetchCategories());
+      }
+    } catch (error) {
+      console.log(error?.message);
+      toast.error("Something is happening!");
+    }
+  };
+
   return (
     <div>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            {" "}
+            <AlertDialogTitle>Task Synchronization</AlertDialogTitle>{" "}
+            <AlertDialogDescription>
+              {" "}
+              {tasks.length > 0 && (
+                <div>
+                  <b className="text-lg">{tasks.length}</b>
+                  Would you like to add?
+                </div>
+              )}
+              {tasks.length === 0 && `There are no tasks!`}
+            </AlertDialogDescription>{" "}
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-red-400"
+              onClick={handleAddBulkTask}
+            >
+              Add
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
       <h1 className="text-5xl p-5">Setting</h1>
       <div className="flex flex-col justify-center items-center pt-4 pb-6 ">
         <p className="text-lg font-bold ">
           Schedule work from other calendars
           <br />
-          Sync:
+          Sync modules:
         </p>
         <div className="flex flex-row justify-between items-center pt-4">
           <img
             className="w-20 h-20 mr-5"
             src="https://i.pinimg.com/originals/6e/09/90/6e099088b3deb805b68d83676af6f067.png"
           />
-          <GoogleOAuthProvider clientId="930538281451-ai6lop8t5knjbh6ce3dj2e8io9kdtp3j.apps.googleusercontent.com">
-            <GoogleLogin
-              text="signin"
-              shape="rectangular"
-              theme="filled_blue"
-            />
-          </GoogleOAuthProvider>
+          <Button
+            className="bg-blue-500 font-semibold"
+            onClick={async () => await responseGoogle()}
+          >
+            Sync 🚀
+          </Button>
         </div>
       </div>
     </div>
--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -2,6 +2,19 @@
 const mongoose = require('mongoose')
 const Task = require('../models/task.model')
 const User = require('../models/user.model')
+const Category = require('../models/category.model')
+
+const { google } = require('googleapis');
+
+const YOUR_CLIENT_ID = '544769320623-1afqfobicnrar2p7j2bf4pghshtl323q.apps.googleusercontent.com'
+const YOUR_CLIENT_SECRET = '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'
+const YOUR_REDIRECT_URI = 'http://localhost:3000/setting'
+
+let oauth2Client = new google.auth.OAuth2(
+    YOUR_CLIENT_ID,
+    YOUR_CLIENT_SECRET,
+    YOUR_REDIRECT_URI
+);
 
 class TaskController {
     index = async (req, res, next) => {
@@ -185,6 +198,123 @@ class TaskController {
             next(error);
         }
     };
+
+    authGoogle = async (req, res, next) => {
+        try {
+            const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+            oauth2Client = new google.auth.OAuth2(
+                YOUR_CLIENT_ID,
+                YOUR_CLIENT_SECRET,
+                'http://localhost:3000/setting'
+            );
+            const url = oauth2Client.generateAuthUrl({
+                access_type: 'offline',
+                scope: SCOPES,
+                prompt: 'consent',
+                include_granted_scopes: true,
+            });
+            return res.json(url)
+        } catch (error) {
+            console.log(error.message);
+            next(error)
+        }
+    }
+
+    googleTask = async (req, res, next) => {
+        try {
+            const { code } = req.query;
+
+            const { tokens } = await oauth2Client.getToken(code)
+            oauth2Client.setCredentials(tokens);
+
+            const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+
+            const colorsRes = await calendar.colors.get();
+            const eventColors = colorsRes.data.event;
+
+            const calendarRes = await calendar.events.list({
+                calendarId: 'primary',
+                timeMin: new Date('2023-01-01T00:00:00Z').toISOString(),
+                timeMax: new Date('2023-12-31T23:59:59Z').toISOString(),
+                singleEvents: true,
+                orderBy: 'startTime',
+            });
+
+            const adjustedEvents = calendarRes.data.items.map(event => {
+                if (event.start.dateTime) {
+                    event.start.dateTime = new Date(new Date(event.start.dateTime).getTime() - 7 * 60 * 60 * 1000).toISOString();
+                }
+
+                if (event.end.dateTime) {
+                    event.end.dateTime = new Date(new Date(event.end.dateTime).getTime() - 7 * 60 * 60 * 1000).toISOString();
+                }
+
+                return event;
+            });
+
+            const filteredEvents = adjustedEvents.filter(event =>
+                event.start.dateTime && event.end.dateTime
+            ).map(event => {
+                const colorId = event.colorId;
+                const color = colorId && eventColors[colorId] ? eventColors[colorId].background : null;
+                return { ...event, color };
+            });
+
+            res.json(filteredEvents);
+        } catch (error) {
+            console.error('Error fetching calendar data:', error);
+            next(error);
+        }
+    };
+
+    storeBulk = async (req, res, next) => {
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
+        try {
+            const tasksData = req.body.tasks;
+            const category = req.params.category;
+
+            if (!Array.isArray(tasksData)) {
+                await session.abortTransaction();
+                session.endSession();
+                return res.status(400).json({
+                    code: "40000",
+                    message: "Invalid input format: Expected an array of tasks"
+                });
+            }
+
+            const newCategory = new Category({
+                name: category,
+                color: "purple",
+                description: "",
+                user: req.userId,
+            });
+
+            await newCategory.save({ session });
+
+            const tasks = tasksData.map(taskData => new Task({
+                ...taskData,
+                user: req.userId,
+                category: newCategory._id
+            }));
+
+            await Task.insertMany(tasks, { session });
+
+            await session.commitTransaction();
+            session.endSession();
+
+            return res.status(201).json({
+                code: "20100",
+                message: "Bulk task creation successful!"
+            });
+        } catch (error) {
+            await session.abortTransaction();
+            session.endSession();
+            next(error);
+        }
+    };
+
 }
 
 module.exports = new TaskController()
